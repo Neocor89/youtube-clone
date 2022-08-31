cc07365b606150839795f0ef206a276fd11a3b2d
Ben <ncor89b@gmail.com>
2022-08-31 13:40:30 +0200

    video section and API test completed

--- youtube-clone2.0/src/components/utils/fetchFromAPI.js
+++ youtube-clone2.0/src/components/utils/fetchFromAPI.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+// const  REACT_APP_API  = process.env.REACT_APP_API;
+// console.log(REACT_APP_API); 
 
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_API,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
